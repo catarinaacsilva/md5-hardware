8886182acc3c6908b02764c1024b6f98d428a870
Catarina Silva <c.alexandracorreia@ua.pt>
2020-06-12 16:08:02 +0100

    Add dma configuration

--- hardwareSoftware.c
+++ hardwareSoftware.c
@@ -9,6 +9,36 @@
 
 // #define N	4000
 
+// DMA configuration
+int DMAConfig(u16 dmaDeviceId, XAxiDma* pDMAInstDefs)
+{
+	XAxiDma_Config* dmaConf;
+	int status;
+
+	dmaConf = XAxiDma_LookupConfig(dmaDeviceId);
+	if (!dmaConf) {
+		xil_printf("No DMA configuration found for %d.\r\n", dmaDeviceId);
+		return XST_FAILURE;
+	}
+
+	status = XAxiDma_CfgInitialize(pDMAInstDefs, dmaConf);
+	if (status != XST_SUCCESS) {
+		xil_printf("DMA Initialization failed %d.\r\n", status);
+		return XST_FAILURE;
+	}
+
+	if (XAxiDma_HasSg(pDMAInstDefs)) {
+		xil_printf("Device configured as SG mode.\r\n");
+		return XST_FAILURE;
+	}
+
+	// Disable interrupts
+	XAxiDma_IntrDisable(pDMAInstDefs, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
+	XAxiDma_IntrDisable(pDMAInstDefs, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
+
+	return XST_SUCCESS;
+}
+
 
 void md5Hw(int* pDst, int* pSrc, unsigned int size)
 {
@@ -79,6 +109,37 @@ int main()
     PrintDataArray(srcData, min(8, N));
     xil_printf("\n\r");
 
+
+	// Configuring DMA
+	xil_printf("\r\nConfiguring DMA...");
+	status = DMAConfig(DMA_DEVICE_ID, &dmaInstDefs);
+	if (status != XST_SUCCESS)
+	{
+		xil_printf("\r\nConfiguration failed.");
+		return XST_FAILURE;
+	}
+	xil_printf("\r\nDMA running...");
+
+
+
+	RestartPerformanceTimer();
+	status = XAxiDma_SimpleTransfer(&dmaInstDefs,(UINTPTR) dstData, N * sizeof(int), XAXIDMA_DEVICE_TO_DMA);
+	status = XAxiDma_SimpleTransfer(&dmaInstDefs,(UINTPTR) srcData, N * sizeof(int), XAXIDMA_DMA_TO_DEVICE);
+	
+	if (status != XST_SUCCESS)
+	{
+		xil_printf("\r\nDMA transfer failed.");
+		return XST_FAILURE;
+	}
+	while ((XAxiDma_Busy(&dmaInstDefs, XAXIDMA_DEVICE_TO_DMA)) || (XAxiDma_Busy(&dmaInstDefs, XAXIDMA_DMA_TO_DEVICE)))
+	{
+		/* Wait */
+	}
+	timeElapsed = StopAndGetPerformanceTimer();
+	xil_printf("\n\rDMA Hardware assisted reverse endianess time: %d microseconds",
+			   timeElapsed / (XPAR_CPU_M_AXI_DP_FREQ_HZ / 1000000));
+	PrintDataArray(dstData, min(8, N));
+	xil_printf("\n\rChecking result: %s\n\r", CheckReversedEndianess(srcData, dstData, N) ? "OK" : "Error");
 
 
     // Hardware assisted
