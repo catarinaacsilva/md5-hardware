9eb0d9025b82f157355a4f90ac7a22640db3ccd4
Catarina Silva <catarinaacorreiasilva@gmail.com>
2020-06-13 10:47:32 +0100

    Fix errors on app

--- Md5Demo/MD5App/src/helloworld.c
+++ Md5Demo/MD5App/src/helloworld.c
@@ -1,60 +1,149 @@
-/******************************************************************************
-*
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
-
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
-
+#include <stdlib.h>
 #include <stdio.h>
 #include "platform.h"
+#include "xparameters.h"
+#include "fsl.h"
+#include "xtmrctr_l.h"
 #include "xil_printf.h"
+
+#include "xaxidma.h"
+
+
+#define N	4000
+
+#define DMA_DEVICE_ID	XPAR_AXIDMA_0_DEVICE_ID
+
+// DMA configuration
+int DMAConfig(u16 dmaDeviceId, XAxiDma* axidma)
+{
+	XAxiDma_Config* dmaConf;
+	int status;
+
+	dmaConf = XAxiDma_LookupConfig(dmaDeviceId);
+	if (!dmaConf) {
+		xil_printf("No DMA configuration found for %d.\r\n", dmaDeviceId);
+		return XST_FAILURE;
+	}
+
+	status = XAxiDma_CfgInitialize(axidma, dmaConf);
+	if (status != XST_SUCCESS) {
+		xil_printf("DMA Initialization failed %d.\r\n", status);
+		return XST_FAILURE;
+	}
+
+	if (XAxiDma_HasSg(axidma)) {
+		xil_printf("Device configured as SG mode.\r\n");
+		return XST_FAILURE;
+	}
+
+	// Disable interrupts
+	XAxiDma_IntrDisable(axidma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
+	XAxiDma_IntrDisable(axidma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
+
+	return XST_SUCCESS;
+}
+
+
+void md5Hw(int* pDst, int* pSrc, unsigned int size)
+{
+	int* p;
+
+	for (p = pSrc; p < pSrc + size; p++, pDst++)
+	{
+		putfslx(*p, 0, FSL_DEFAULT);
+		getfslx(*pDst, 0, FSL_DEFAULT);
+	}
+}
+
+
+void PrintDataArray(int* pData, unsigned int size)
+{
+	int* p;
+
+	xil_printf("\n\r");
+	for (p = pData; p < pData + size; p++)
+	{
+		xil_printf("%08x  ", *p);
+	}
+}
+
+void ResetPerformanceTimer()
+{
+	XTmrCtr_Disable(XPAR_TMRCTR_0_BASEADDR, 0);
+	XTmrCtr_SetLoadReg(XPAR_TMRCTR_0_BASEADDR, 0, 0x00000001);
+	XTmrCtr_LoadTimerCounterReg(XPAR_TMRCTR_0_BASEADDR, 0);
+	XTmrCtr_SetControlStatusReg(XPAR_TMRCTR_0_BASEADDR, 0, 0x00000000);
+}
+
+void RestartPerformanceTimer()
+{
+	ResetPerformanceTimer();
+	XTmrCtr_Enable(XPAR_TMRCTR_0_BASEADDR, 0);
+}
+
+unsigned int GetPerformanceTimer()
+{
+	return XTmrCtr_GetTimerCounterReg(XPAR_TMRCTR_0_BASEADDR, 0);
+}
+
+unsigned int StopAndGetPerformanceTimer()
+{
+	XTmrCtr_Disable(XPAR_TMRCTR_0_BASEADDR, 0);
+	return GetPerformanceTimer();
+}
 
 
 int main()
 {
+	//int srcData[N], dstData[N];
+	uint32_t msgInput, msgOutput; // check if it is correct
+	unsigned int timeElapsed;
+
+	int status;
+
+	XAxiDma axiDma;
+
     init_platform();
 
-    print("Hello World\n\r");
+
+    RestartPerformanceTimer();
+    srand(0);
+
+    msgInput = rand();
+
+    timeElapsed = StopAndGetPerformanceTimer();
+    xil_printf("\n\rArray initialization time: %d microseconds\n\r",
+    		   timeElapsed / (XPAR_CPU_M_AXI_DP_FREQ_HZ / 1000000));
+    // PrintDataArray(srcData, min(8, N));
+    xil_printf("\n\r");
+
+
+	// Configuring DMA
+	xil_printf("\r\nConfiguring DMA");
+	status = DMAConfig(DMA_DEVICE_ID, &axiDma);
+	if (status != XST_SUCCESS)
+	{
+		xil_printf("\r\nConfiguration failed");
+		return XST_FAILURE;
+	}
+	xil_printf("\r\nDMA running");
+
+	// MD5
+	RestartPerformanceTimer();
+	status = XAxiDma_SimpleTransfer(&axiDma,(UINTPTR) msgOutput, N * sizeof(int), XAXIDMA_DEVICE_TO_DMA);
+	status = XAxiDma_SimpleTransfer(&axiDma,(UINTPTR) msgInput, N * sizeof(int), XAXIDMA_DMA_TO_DEVICE);
+
+	if (status != XST_SUCCESS)
+	{
+		xil_printf("\r\nDMA transfer failed");
+		return XST_FAILURE;
+	}
+	while ((XAxiDma_Busy(&axiDma, XAXIDMA_DEVICE_TO_DMA)) || (XAxiDma_Busy(&axiDma, XAXIDMA_DMA_TO_DEVICE))) { }
+
+	timeElapsed = StopAndGetPerformanceTimer();
+	xil_printf("\n\rDMA Hardware md5 time: %d microseconds", timeElapsed / (XPAR_CPU_M_AXI_DP_FREQ_HZ / 1000000));
+	// PrintDataArray(dstData, min(8, N));
+
 
     cleanup_platform();
     return 0;
